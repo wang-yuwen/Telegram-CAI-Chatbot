eb570a5d2ed52f4a91b40725649716d2c142b748
Wang-Yubang <142516303+Wang-Yubang@users.noreply.github.com>
2023-08-22 16:38:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const {Telegraf} = require('telegraf');
-const bot = new Telegraf('6651025524:AAGzTiP03tMoh6rX80BGHUS8s5diWTBIUbk'); //bot token
+const bot = new Telegraf('СЮДА ВСТАВИТЬ ТОКЕН ТЕЛЕГРАМ БОТА'); //bot token
 const CharacterAI = require('node_characterai');
 const characterAI = new CharacterAI();
 
 (async () => {
-    await characterAI.authenticateWithToken('eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkVqYmxXUlVCWERJX0dDOTJCa2N1YyJ9.eyJpc3MiOiJodHRwczovL2NoYXJhY3Rlci1haS51cy5hdXRoMC5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMTUxMzI2MDAzNTc1ODQ4NDMzMjgiLCJhdWQiOlsiaHR0cHM6Ly9hdXRoMC5jaGFyYWN0ZXIuYWkvIiwiaHR0cHM6Ly9jaGFyYWN0ZXItYWkudXMuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY5MjIxMzIyOCwiZXhwIjoxNjk0ODA1MjI4LCJhenAiOiJkeUQzZ0UyODFNcWdJU0c3RnVJWFloTDJXRWtucVp6diIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwifQ.ggFHi--2LWhqy1zmntc3CViw2qBrDJ0QoUPeeR5iS7svSSC1abrHINauqYuYgCWYL3pKFv4cs09IXKOZOgtEsFHCu6u4ohm4k1wXSH4WTFxfhPUGiFxKjHRkWAfwoXYggHqW7aFq2us5divwUpZlqY0nSfxOxR-6jFELx1Je5v3qzUUbRy-VHKp-RssHhG5Ha3k7AJoc7feyoafx0n8iC7C7fnYmeF42Zu9renJKEVPJyLoTt-7XdwTZfAWGmr2ZE6Yfnq94Tth0jQNRZW-XYy6RatWeQedm3NKMChgqpRYQAwSm84smjcTz1Z8fryr0G82xCvbtBRuOXFdHrlLbLA'); //account token or using guest authenticateAsGuest() 
-    const characterId = "-XJ84xEDf5jf7hYPP1YRulYkKZaoaRPwWgQa4zqwtxE" //characterid token
+    await characterAI.authenticateWithToken('СЮДА ВСТАВИТЬ ТОКЕН CHARACTER AI'); //account token or using guest authenticateAsGuest() 
+    const characterId = "СЮДА ВСТАВИТЬ ID БОТА" //characterid token
     const chat = await characterAI.createOrContinueChat(characterId);
 
     bot.start((ctx) => {
-        ctx.reply('Сап, я Игорь Духанин, тру стори, пруфов не будет')
+        ctx.reply('ТЕКСТ ПРИ СТАРТЕ БОТА')
     });
 
-    bot.hears(/Игорь|игорь|ОП|Оп|оп|Духанин|духанин|DukhAInin_bot|Dukhan1n|Духан|Игорян|Нейродухан|Нейродуханин|НейроДухан|Духан/, async (ctx) => {
+    bot.hears(/ВСЕ|ВАРИАНТЫ|ОБРАЩЕНИЙ|ПРИ КОТОРЫХ|БОТ|ОТВЕТИТ НА СООБЩЕНИЕ/, async (ctx) => {
         const context = ctx.from.first_name;
         const userText = ctx.message.text;
         const textRequest = `${context}: ${userText}`;

4446519dc5920a3f3c859671148e7c26c20b8a53
Wang-Yubang <142516303+Wang-Yubang@users.noreply.github.com>
2023-09-06 23:53:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
 const {Telegraf} = require('telegraf');
-const bot = new Telegraf('СЮДА ВСТАВИТЬ ТОКЕН ТЕЛЕГРАМ БОТА'); //bot token
+const bot = new Telegraf('ТОКЕН БОТА'); //bot token
 const CharacterAI = require('node_characterai');
 const characterAI = new CharacterAI();
 
 (async () => {
-    await characterAI.authenticateWithToken('СЮДА ВСТАВИТЬ ТОКЕН CHARACTER AI'); //account token or using guest authenticateAsGuest() 
-    const characterId = "СЮДА ВСТАВИТЬ ID БОТА" //characterid token
+    await characterAI.authenticateWithToken('ТОКЕН CAI'); //account token or using guest authenticateAsGuest() 
+    const characterId = "ID БОТА В CAI" //characterid token
     const chat = await characterAI.createOrContinueChat(characterId);
 
     bot.start((ctx) => {
-        ctx.reply('ТЕКСТ ПРИ СТАРТЕ БОТА')
+        ctx.reply('СТАРТОВОЕ СООБЩЕНИЕ')
     });
 
-    bot.hears(/ВСЕ|ВАРИАНТЫ|ОБРАЩЕНИЙ|ПРИ КОТОРЫХ|БОТ|ОТВЕТИТ НА СООБЩЕНИЕ/, async (ctx) => {
+     bot.command('rpl', async(ctx) => {
+        const context2 = ctx.message.reply_to_message.from.first_name;
+        const userText2 = ctx.message.reply_to_message.text;
+        const textRequest = `${context2}: ${userText2}`;
+        const response = await chat.sendAndAwaitResponse(textRequest, true) 
+        ctx.reply(response, {
+            reply_to_message_id: ctx.message.reply_to_message.message_id
+            });
+    });
+
+    bot.hears(/ВСЕ|ВАРИАНТЫ|ОБРАЩЕНИЙ|К|БОТУ/, async (ctx) => {
         const context = ctx.from.first_name;
         const userText = ctx.message.text;
         const textRequest = `${context}: ${userText}`;
         const response = await chat.sendAndAwaitResponse(textRequest, true)
-        ctx.reply(response)
+        ctx.reply(response, {
+            reply_to_message_id: ctx.message.message_id
+        });
     });
 
-    bot.hears(/Бот, завали ебало/, async (ctx) => {
+    bot.hears(/Бот, завали ебало|БЗЕ|Бот завали ебало/, async(ctx) => {
         const Refresh = await chat.saveAndStartNewChat()
         ctx.Refresh
-        ctx.reply('Братишка, ну не стукай')
+        ctx.reply('Братишка, ну не стукай', {
+            reply_to_message_id: ctx.message.message_id
+        });
     });
 
 })();
 
 bot.launch()
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
